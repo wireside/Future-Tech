50d5c77a1b6c3d142bd7a076fb66915252f27513
Bogdan Salaev <wiresifed@gmail.com>
2025-02-20 23:48:35 +0300

    Added js logic for expandable-content elements

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,9 @@
 import Header from './Header.js'
 import TabsCollection from "./Tabs.js";
 import VideoPlayerCollection from "./VideoPlayer.js";
+import ExpandableContentCollection from "./ExpandableContent.js";
 
 new Header()
 new TabsCollection()
 new VideoPlayerCollection()
+new ExpandableContentCollection()
